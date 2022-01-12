91dc3b79cfdb000ce304764d0d144ef1dc489353
abnerwerley <abnerwerley77@gmail.com>
2022-01-12 12:03:06 -0300

    @abnerwerley[Basic POST, PUT and DELETE methods configured and tested]

--- src/main/java/com/firedevit/controller/UsuarioController.java
+++ src/main/java/com/firedevit/controller/UsuarioController.java
@@ -5,8 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,10 +35,24 @@ public class UsuarioController {
 				.orElse(ResponseEntity.notFound().build());
 	}
 	
-	@GetMapping("/{userName}")
+	@GetMapping("/in/{userName}")
 	public ResponseEntity<Usuario> getByUserName(@PathVariable String nome){
 		return repository.findByNomeContainingIgnoreCase(nome).map(resp ->ResponseEntity.ok(resp))
 				.orElse(ResponseEntity.notFound().build());
 	}
 	
+	@PostMapping("/cadastrar")
+	public ResponseEntity<Usuario> cadastrar(@RequestBody Usuario usuario){
+		return ResponseEntity.ok(repository.save(usuario));
+	}
+	
+	@PutMapping("/atualizar")
+	public ResponseEntity<Usuario> atualizar(@RequestBody Usuario usuario){
+		return ResponseEntity.ok(repository.save(usuario));
+	}
+	
+	@DeleteMapping("/deletar/{id}")
+	public void deletaUsuario(@PathVariable long id) {
+		repository.deleteById(id);
+	}
 }
