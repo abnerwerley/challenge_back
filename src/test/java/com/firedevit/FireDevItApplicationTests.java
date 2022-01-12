22b35ce7da3d8e3464feb2b933ce88bc68ed2734
abnerwerley <abnerwerley77@gmail.com>
2022-01-11 22:49:52 -0300

    @abnerwerley[Task: Project Configured]

---
+++ src/test/java/com/firedevit/FireDevItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firedevit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FireDevItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
